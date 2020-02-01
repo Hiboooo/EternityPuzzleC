a009ca9c437ad96deecf3512b6c8c0a322e6d135
Dupré Thibault <thibault.dupre.1999@gmail.com>
2020-02-01 17:08:50 +0100

    ajout de la fonction qui retourne la case correspondante a une case donné

--- EternityPuzzle.c
+++ EternityPuzzle.c
@@ -5,6 +5,7 @@
 #define NIVEAU 6 // 4-7
 
 void generePlateau(int[NIVEAU][NIVEAU][4]);
+int* corresp(int[NIVEAU][NIVEAU][4],int, int, int);
 void affichePlateau(int[NIVEAU][NIVEAU][4]);
 char getChar(int);
 
@@ -16,7 +17,14 @@ int main(int argc, char *argv[])
   printf("Par Quentin Germain et Thibault Dupré\n");
 
   int tab[NIVEAU][NIVEAU][4];
+
   generePlateau(tab);
+  affichePlateau(tab);
+
+  tab[5][5][3] = 1;
+  affichePlateau(tab);
+
+  *corresp(tab,5,5,3) = 1;
   affichePlateau(tab);
 
   putchar('\n');
@@ -26,7 +34,7 @@ void affichePlateau(int plateau[NIVEAU][NIVEAU][4])
 {
   printf("\n   ");
   for (int t=0; t<NIVEAU; t++)
-    printf(" %d ",t);
+    printf(" %d ",t+1);
   putchar('\n');
   for (int t=0; t<NIVEAU; t++)
   {
@@ -59,5 +67,19 @@ void generePlateau(int plateau[NIVEAU][NIVEAU][4])
   for (int t=0; t<NIVEAU; t++)
     for (int h=0; h<NIVEAU; h++)
       for (int i=0; i<4; i++)
-        plateau[t][h][i] = (rand() % (3 - 0 + 1)) + 0;;
+        plateau[t][h][i] = 0;
+}
+
+int* corresp(int plateau[NIVEAU][NIVEAU][4],int x, int y, int z)
+{
+  if (z==0)
+    return &(plateau[x][(y-1)%NIVEAU][(z+2)%4]);
+  else if (z==1)
+    return &(plateau[(x+1)%NIVEAU][y][(z+2)%4]);
+  else if (z==2)
+    return &(plateau[x][(y+1)%NIVEAU][(z+2)%4]);
+  else if (z==3)
+    return &(plateau[(x-1)%NIVEAU][y][(z+2)%4]);
+  else
+    return NULL;
 }
