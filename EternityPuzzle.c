b66c48385190886d37a802c365ebc6aaaa38a332
Dupré Thibault <thibault.dupre.1999@gmail.com>
2020-02-01 20:42:16 +0100

    Generation du plateau

--- EternityPuzzle.c
+++ EternityPuzzle.c
@@ -4,9 +4,15 @@
 
 #define NIVEAU 6 // 4-7
 
+// genere le plateau
 void generePlateau(int[NIVEAU][NIVEAU][4]);
+// recherche la premiere place vide d'une piece (-1 si aucune place)
+int premierePlace(int[4]);
+// retourne la case mémoire de la case correspondante a la case de coordonné x y z
 int* corresp(int[NIVEAU][NIVEAU][4],int, int, int);
+// affiche le plateau
 void affichePlateau(int[NIVEAU][NIVEAU][4]);
+// converti les datas pour l'affichage
 char getChar(int);
 
 int main(int argc, char *argv[])
@@ -16,16 +22,10 @@ int main(int argc, char *argv[])
   printf("Projet de licence 2\n");
   printf("Par Quentin Germain et Thibault Dupré\n");
 
-  int tab[NIVEAU][NIVEAU][4];
+  int plateau[NIVEAU][NIVEAU][4];
 
-  generePlateau(tab);
-  affichePlateau(tab);
-
-  tab[5][5][3] = 1;
-  affichePlateau(tab);
-
-  *corresp(tab,5,5,3) = 1;
-  affichePlateau(tab);
+  generePlateau(plateau);
+  affichePlateau(plateau);
 
   putchar('\n');
 }
@@ -58,7 +58,8 @@ char getChar(int t)
     case 0: return 'A';
     case 1: return 'B';
     case 2: return 'C';
-    default: return 'D';
+    case 3: return 'D';
+    default: return 'X';
   }
 }
 
@@ -67,19 +68,40 @@ void generePlateau(int plateau[NIVEAU][NIVEAU][4])
   for (int t=0; t<NIVEAU; t++)
     for (int h=0; h<NIVEAU; h++)
       for (int i=0; i<4; i++)
-        plateau[t][h][i] = 0;
+        plateau[t][h][i] = -1; // init le tableau a vide
+  for (int t=0; t<NIVEAU; t++)
+    for (int h=0; h<NIVEAU; h++)
+      while(premierePlace(plateau[t][h])!=-1) // tant qu'il y a de la place
+      {
+        int n = rand()%4; // prend une data de maniere random
+        int prpl = premierePlace(plateau[t][h]); // recupaire la premiere place vide de la piece
+        plateau[t][h][prpl] = n; // met la data dans a cette place
+        *corresp(plateau,t,h,prpl) = n; // met aussi la data dans la case correspondante
+      }
+}
+
+int premierePlace(int piece[4])
+{
+  for (int t=0; t<4; t++)
+    if (piece[t]==-1)
+      return t;
+  return -1;
 }
 
 int* corresp(int plateau[NIVEAU][NIVEAU][4],int x, int y, int z)
 {
-  if (z==0)
-    return &(plateau[x][(y-1)%NIVEAU][(z+2)%4]);
-  else if (z==1)
-    return &(plateau[(x+1)%NIVEAU][y][(z+2)%4]);
-  else if (z==2)
-    return &(plateau[x][(y+1)%NIVEAU][(z+2)%4]);
-  else if (z==3)
-    return &(plateau[(x-1)%NIVEAU][y][(z+2)%4]);
-  else
-    return NULL;
+  switch(z)
+  {
+    case 0: y = (y==0)? NIVEAU-1: y-1;
+            break;
+    case 1: x = (x==NIVEAU-1)? 0: x+1;
+            break;
+    case 2: y = (y==NIVEAU-1)? 0: y+1;
+            break;
+    case 3: x = (x==0)? NIVEAU-1: x-1;
+  }
+
+  z = (z<2)? z+2: z-2;
+
+  return &(plateau[x][y][z]);
 }
